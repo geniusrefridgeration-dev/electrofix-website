4ceb41eaad0095d634ff883c2db9a36858d28666
test_user <test_user@.com>
2026-06-19 18:25:03 +0530

    robots error

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -4,6 +4,5 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: [{ userAgent: '*', allow: '/', disallow: ['/api/', '/bookings', '/profile'] }],
     sitemap: `${BASE}/sitemap.xml`,
-    host: BASE,
   }
 }
